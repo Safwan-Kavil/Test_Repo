d6694cb5bf8ee48537961540515afc98e5f6f6d9
Safwan Abdurahiman Kavil <157449123+safwan-sk2110900@users.noreply.github.com>
2024-05-13 01:23:13 +0300

    ch

--- Project_Phase2_VERCEL/web-app/next.config.mjs
+++ Project_Phase2_VERCEL/web-app/next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+export default nextConfig;
 
-export default {
-    experimental: {
-      serverless: true,
-    },
-    // Other configuration options can go here if needed
-  };
-  
